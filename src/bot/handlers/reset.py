510d52969eb5d3f41c4893623b8c66334fd9b017
Hitalo M. <hitalo331@outlook.com>
2025-04-12 13:49:03 -0300

    feat: add resetall command

--- src/bot/handlers/reset.py
+++ src/bot/handlers/reset.py
@@ -5,6 +5,8 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from bot.database.models import Conversation
+from bot.filters.sudo import SudoFilter
 from bot.utils.chat.history import clear_conversation_history
 
 router = Router(name="reset")
@@ -17,3 +19,9 @@ async def reset_handler(message: Message) -> None:
 
     await clear_conversation_history(message.from_user.id)
     await message.answer("History cleared.")
+
+
+@router.message(Command(commands=["resetall", "fuckall"]), SudoFilter())
+async def reset_all_handler(message: Message) -> None:
+    await Conversation.all().delete()
+    await message.answer("History cleared for all users!")
